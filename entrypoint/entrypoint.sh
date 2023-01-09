e70247f16df19a0aa8b3da3f352d132937571c34
BFG <Tom.Fredrik@blenning.no>
2023-01-09 18:15:36 +0100

    A bit of sanity checking. Is GIT_SOURCE set?

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -2,8 +2,6 @@
 
 SOURCE_DIR=/var/www/localhost/htdocs
 
-(cd /opt/python-github-webhook && python /opt/python-github-webhook/run.py) &
-
 if test -n "$(find /entrypoint/pre.d/  -maxdepth 1 -type f -print -quit)"
 then
   for file in /entrypoint/pre.d/*
@@ -29,16 +27,31 @@ then
   REINIT=true
 fi
 
+REPO_INITIALIZED=false
 if $REINIT
 then
    echo CLEANING OLD FILES
    rm -rfv $SOURCE_DIR/.[!.]* $SOURCE_DIR/..?* $SOURCE_DIR/*
-   echo INITIALIZING REPOSITORY
-   git clone --recurse-submodules -j8 $GIT_SOURCE $SOURCE_DIR
-   echo REPOSITORY INITIALIZED
+   REPO_INITIALIZED=false
+   if [ -n "$GIT_SOURCE" ]
+   then
+     echo INITIALIZING REPOSITORY
+     git clone --recurse-submodules -j8 $GIT_SOURCE $SOURCE_DIR && REPO_INITIALIZED=true
+   fi
+   echo RR $REPO_INITIALIZED
+   if $REPO_INITIALIZED
+   then
+     echo REPOSITORY INITIALIZED
+     (cd /opt/python-github-webhook && python /opt/python-github-webhook/run.py) &
+   else
+     echo WARNING: REPOSITORY NOT INITIALIZED
+     echo WARNING: This is not supposed to happen
+     echo "WARNING: Did you set GIT_SOURCE properly?"
+   fi
 else
    echo Using already exisiting source
    (cd $SOURCE_DIR && git pull)
+   REPO_INITIALIZED=true
 fi
 
 if [ -z "$HOOKS" ]
@@ -48,11 +61,14 @@ fi
 
 hookfile=/etc/apache2/conf.d/hooks.conf
 
-for hook in $HOOKS
-do
-  echo ProxyPass /$hook http://localhost:5000/$hook | tee -a $hookfile
-  echo ProxyPassReverse /$hook http://localhost:5000/$hook  | tee -a $hookfile
-done
+if $REPO_INITIALIZED
+then
+  for hook in $HOOKS
+  do
+    echo ProxyPass /$hook http://localhost:5000/$hook | tee -a $hookfile
+    echo ProxyPassReverse /$hook http://localhost:5000/$hook  | tee -a $hookfile
+  done
+fi
 
 if test -n "$(find /entrypoint/post.d/  -maxdepth 1 -type f -print -quit)"
 then
